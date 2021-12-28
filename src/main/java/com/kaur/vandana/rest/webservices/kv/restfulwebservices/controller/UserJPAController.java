8ae78ed236a59a862064c96de46771e8a6df58f8
K Vandana <kaurvandana94@gmail.com>
2021-12-28 23:44:22 +0530

    Update

--- src/main/java/com/kaur/vandana/rest/webservices/kv/restfulwebservices/controller/UserJPAController.java
+++ src/main/java/com/kaur/vandana/rest/webservices/kv/restfulwebservices/controller/UserJPAController.java
@@ -21,7 +21,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-public class UserJPAResource {
+public class UserJPAController {
 
     @Autowired
     private UserRepository userRepository;
